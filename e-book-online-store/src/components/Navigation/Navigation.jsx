f7d059466efd1d06cc72e96b87842af49b426a15
viktor-dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2023-10-30 21:43:01 +0200

    add basic Navigation componenet

---
+++ e-book-online-store/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+
+
+export default function Navigation() {
+
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/register">Register</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+
+
+}
