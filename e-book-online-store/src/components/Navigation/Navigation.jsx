8dfce2825bf6bcfa71fb89be075f073bfbdfa56b
viktor-dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2023-11-02 21:39:24 +0200

    create Navigation component

--- e-book-online-store/src/components/Navigation/Navigation.jsx
+++ e-book-online-store/src/components/Navigation/Navigation.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 
+import  './Navigation.css';
 
 export default function Navigation() {
 
@@ -7,15 +8,26 @@ export default function Navigation() {
     return (
         <nav>
             <ul>
+
                 <li>
-                    <Link to="/">Home</Link>
+                    <Link to="/categories">Категории</Link>
                 </li>
                 <li>
-                    <Link to="/register">Register</Link>
+                    <Link to="/news">Новини</Link>
                 </li>
                 <li>
-                    <Link to="/login">Login</Link>
+                    <Link to="/sell">Продай книга</Link>
                 </li>
+                <li>
+                    <Link to="/contacts">Контакти</Link>
+                </li>
+                <li>
+                    <Link to="/login"> <i class="fa-solid fa-circle-user"></i> Log in </Link>
+                </li>
+                <li>
+                    <Link to="/cart"> <i class="fa-solid fa-cart-shopping"></i> </Link>
+                </li>
+
             </ul>
         </nav>
     )
