89bac44db21cabfcdfdac4f8e364e32324b6ce9f
viktor-dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2023-11-04 18:46:25 +0200

    hook for saving user information in localStorage

---
+++ e-book-online-store/src/hooks/useLocalStorage.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+
+
+export const useLocalStorage = (key, initialValue) => {
+    const [state, setState] = useState(() => {
+        const localStorageState = localStorage.getItem(key);
+        if(localStorageState){
+            const persistedState = JSON.parse(localStorageState);
+            return persistedState
+        }
+        return initialValue
+    });
+
+    const setLocalStorageState =  (value) => {
+        setState(value);
+
+        localStorage.setItem(key, JSON.stringify(value))
+    }
+
+
+    return [
+        state,
+        setLocalStorageState
+    ]
+} 
