7519f803636ff729e020b00cb6b4368d3105f4ac
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2023-11-04 15:43:14 +0200

    Delete DemoEbooks directory

--- DemoEbooks/common/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from DemoEbooks.common import views
-
-urlpatterns = [
-    path('', views.homepage, name='homepage'),
-    path('about/', views.about_us, name='about'),
-    path('contacts/', views.contact_us, name='contacts'),
-]
--- DemoEbooks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('DemoEbooks.common.urls')),
-    path('accounts/', include('DemoEbooks.accounts.urls')),
-    path('books/', include('DemoEbooks.books.urls')),
-]
