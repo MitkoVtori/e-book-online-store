6be551871ad3721ef1e04078bc09cd2e102f9d86
viktor-dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2023-11-04 15:11:52 +0200

    create login form

--- e-book-online-store/src/components/Login/Login.jsx
+++ e-book-online-store/src/components/Login/Login.jsx
@@ -1,11 +1,52 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
+import styles from './Login.module.css'
 
 
 export default function Login () {
 
+    const [formValues, setFormValues] = useState({
+        email: '',
+        password: ''
+    })
+
+
+    const changeHandler = (e) => {
+        setFormValues( state => ({...state, [e.target.name]: e.target.value}))
+    }
+
 
     return(
-        <h2>Login Component</h2>
+       
+        <div className={styles['form-container']}>
+
+
+            <div className={styles['logo']}>
+                <h2>Logo here</h2>
+            </div>
+
+            <form method="POST">
+
+                <p className={styles['sign-on']}>Sign on</p>
+
+                
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" value={formValues.email} onChange={changeHandler} />
+
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" value={formValues.password} onChange={changeHandler} />
+
+                <Link to="/">Forgot your password?</Link>
+
+                <input type="submit" value="Sign on" />
+
+
+                <Link to="/">Create account</Link>
+
+            </form>
+
+        </div>
     )
 
 
