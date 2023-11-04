536dc793ebaeca680c832f31e959be88c112ef6f
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2023-11-04 15:43:04 +0200

    Delete merge directory

--- merge/e_books_online_store/Profile/validators.py
+++
@@ -1,7 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def only_letters(value):
-    for symbol in value:
-        if not symbol.isalpha():
-            raise ValidationError('Use only alphabetical letters!')
