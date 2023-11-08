4a6ede6ec7b8d27a873b5d892ed9382e4aa89a65
viktor-dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2023-11-08 22:45:02 +0200

    Create Array with books for testing , model is not correct

---
+++ e-book-online-store/src/utils/testConstants/testBooksArray.js
@@ -0,0 +1,28 @@
+export const tempBooks = [
+    {
+      "author": "Chinua Achebe",
+      "country": "Nigeria",
+      "imageLink": "images/things-fall-apart.jpg",
+      "language": "English",
+      "pages": 209,
+      "title": "Things Fall Apart",
+      "year": 1958
+    },
+    {
+      "author": "Hans Christian Andersen",
+      "country": "Denmark",
+      "imageLink": "images/fairy-tales.jpg",
+      "language": "Danish",
+      "pages": 784,
+      "title": "Fairy tales",
+      "year": 1836
+    },
+    {
+      "author": "Dante Alighieri",
+      "country": "Italy",
+      "imageLink": "images/the-divine-comedy.jpg",
+      "language": "Italian",
+      "pages": 928,
+      "title": "The Divine Comedy",
+      "year": 1315
+    },]
