24e9917513c9a25bed5a5c5cea474d8161b900ee
viktor-dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2023-11-08 22:45:32 +0200

    add BookProvider in App

--- e-book-online-store/src/App.jsx
+++ e-book-online-store/src/App.jsx
@@ -1,31 +1,38 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from "./contexts/AuthContext.jsx";
+import { BookProvider } from './contexts/BookContext.jsx';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
 import Header from './components/Header/Header';
 import HomePage from './components/HomePage/HomePage';
 import Footer from './components/Footer/Footer.jsx';
+import CategoriesPage from './components/Categories/CategoriesPage/CategoriesPage.jsx';
+
 
 function App() {
 
   return (
     <>
-   
-        <BrowserRouter>
+
+      <BrowserRouter>
         < AuthProvider >
-          <Header />
+          <BookProvider>
 
+            <Header />
 
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-          </AuthProvider>
-        </BrowserRouter>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/categories" element={<CategoriesPage />} />
+            </Routes>
+            
+          </BookProvider>
+        </AuthProvider>
+      </BrowserRouter>
 
-        <Footer />
-     
+      <Footer />
+
 
     </>
   )
