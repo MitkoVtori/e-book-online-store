2f80b5d94e369e82e56279e40f886a11f130d19e
TjDimitrov <krisper31@gmail.com>
2023-11-10 13:04:06 +0200

    acc views

---
+++ e_books_online_store/accounts/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from e_books_online_store.accounts import views
+
+urlpatterns = [
+    path('register/', views.RegisterUserView.as_view(), name='register'),
+    path('login/', views.LoginUserView.as_view(), name='login'),
+    path('logout/', views.LogoutUserView.as_view(), name='logout'),
+    path('profile/<int:pk>/', include([
+        path('', views.UserDetailsView.as_view(), name='profile-details'),
+        path('edit/', views.EditUserView.as_view(), name='profile-edit'),
+        path('delete/', views.DeleteUserView.as_view(), name='profile-delete'),
+        path('change-password/', views.ChangePasswordView.as_view(), name='change-password')
+    ]))
+]
--- e_books_online_store/accounts/views.py
+++ e_books_online_store/accounts/views.py
@@ -1,3 +1,75 @@
-from django.shortcuts import render
+from django.contrib.auth import login, get_user_model, logout
+from django.core.validators import EmailValidator
 
-# Create your views here.
+from rest_framework.generics import RetrieveAPIView, DestroyAPIView, UpdateAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.authentication import SessionAuthentication
+from .models import *
+from .serializer import *
+
+UserModel = get_user_model()
+
+
+class RegisterUserView(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request):
+        clean_data = request.data
+        serializer = UserRegistrationSerializer(data=clean_data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.create(clean_data)
+            if user:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginUserView(APIView):
+    permission_classes = (permissions.AllowAny,)
+    authentication_classes = (SessionAuthentication,)
+
+    def post(self, request):
+        data = request.data
+        assert EmailValidator(data)
+        serializer = UserLoginSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.check_user(data)
+            login(request, user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class LogoutUserView(APIView):
+    def post(self, request):
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
+
+
+class UserDetailsView(RetrieveAPIView):
+    queryset = UserModel.objects.all()
+    serializer_class = UserDetailsSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_field = 'pk'
+
+
+class EditUserView(UpdateAPIView):
+    queryset = UserModel.objects.all()
+    serializer_class = UpdateUserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+
+class DeleteUserView(DestroyAPIView):
+    queryset = UserModel.objects.all()
+    serializer_class = UserDetailsSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_field = 'pk'
+
+
+class ChangePasswordView(UpdateAPIView):
+    queryset = UserModel.objects.all()
+    permission_classes = [IsAuthenticated]
+    serializer_class = ChangePasswordSerializer
