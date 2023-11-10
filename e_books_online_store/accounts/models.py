086227a968e1efc1f776e7a0151d1d1dd0a54e8d
TjDimitrov <krisper31@gmail.com>
2023-11-10 14:40:55 +0200

    Custom User Manager

--- e_books_online_store/accounts/models.py
+++ e_books_online_store/accounts/models.py
@@ -1,10 +1,43 @@
 from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin, UserManager
 from django.core.validators import MinLengthValidator
 
 from e_books_online_store import books
 from e_books_online_store.accounts.validators import only_letters, image_size
+
+
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The given email must be set')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must have is_staff=True.'
+            )
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must have is_superuser=True.'
+            )
+        return self._create_user(email, password, **extra_fields)
 
 
 class StoreUser(AbstractBaseUser, PermissionsMixin):
@@ -64,4 +97,6 @@ class StoreUser(AbstractBaseUser, PermissionsMixin):
 
     date_joined = models.DateTimeField(auto_now_add=True)
 
-    objects = UserManager()
+    objects = CustomUserManager()
+
+
