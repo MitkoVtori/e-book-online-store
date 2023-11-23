7fbbc0d96bda44a481fb09913566bfa1fa0a0f4e
viktor-dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2023-11-23 20:54:00 +0200

    details page update

--- e-book-online-store/src/components/Book/BookDetails/BookDetails.jsx
+++ e-book-online-store/src/components/Book/BookDetails/BookDetails.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { getOneBook } from "../../../services/bookService";
 
+import styles from "./BookDetails.module.css"
 
 export default function BookDetails() {
 
@@ -20,16 +21,46 @@ export default function BookDetails() {
     console.log(book)
 
     return(
-        <>
-        <h1> { book.title }</h1>
-        <h2> { book.author } </h2>
-        <img src={book.cover_image} alt="book" />
 
-        <h3> { book.description } </h3>
-        <h4> { book.price } </h4>
-        <h5> { book.publication_date } </h5>
 
-        </>
+        <div  className={styles['details-container']} >
+
+            <div className={styles['img-container']}>
+                <img src={book.cover_image} alt="book_cover" />
+            </div>
+
+          
+
+            <div className={styles['info-container']}>
+
+            <p className={styles['book-name']} > { book.title } </p>
+            <p className={styles['book-author']}> { book.author } </p>
+            <p className={styles['book-price']}> {book.price} лв. </p>
+
+
+            <div className={styles['btns-container']}>
+                    <button className={styles['add-btn']}>Add to Cart</button>
+                </div>
+
+            
+            </div>
+
+      
+                
+
+        </div>
+
+
+        // <>
+        // <h1> { book.title }</h1>
+        // <h2> { book.author } </h2>
+        // <img src={book.cover_image} alt="book" />
+
+        // <h3> { book.description } </h3>
+        // <h4> { book.price } </h4>
+        // <h5> { book.publication_date } </h5>
+
+        // </>
     )
 }
 
