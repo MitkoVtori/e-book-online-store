f324fc9ebca7d708eb0514d498e30336b9d6432d
Viktor Dremsizov <viktorkz91@gmail.com>
2024-01-07 18:07:52 +0200

    I add some validation for Login and Register

--- e-book-online-store/src/components/Login/Login.jsx
+++ e-book-online-store/src/components/Login/Login.jsx
@@ -2,56 +2,114 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useAuthContext } from "../../contexts/AuthContext";
 
-import styles from './Login.module.css'
+import styles from "./Login.module.css";
+
+export default function Login() {
+  document.title = "Login";
 
 
-export default function Login () {
+  const { onLoginSubmit } = useAuthContext();
 
-    const { onLoginSubmit } = useAuthContext();
+  const [errors, setErrors] = useState({});
 
-    const [formValues, setFormValues] = useState({
-        email: '',
-        password: ''
-    })
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const changeHandler = (e) => {
+    setFormValues((state) => ({ ...state, [e.target.name]: e.target.value }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onLoginSubmit(formValues);
+  };
 
 
-    const changeHandler = (e) => {
-        setFormValues( state => ({...state, [e.target.name]: e.target.value}))
+//   Server validator
+//   const [hasServerError, setHasServerError] = useState(false);
+//   const [serverError, setServerError] = useState({});
+
+  ///////////////////////////////////////////////////////// Validations
+
+  function validateEmail(email) {
+    const emailRegex = /^[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]+$/;
+    return emailRegex.test(email);
+  }
+
+  const emailValidator = () => {
+    if (!validateEmail(formValues.email)) {
+      setErrors((state) => ({
+        ...state,
+        email: "Посоченият от вас мейл адрес не е във валиден формат",
+      }));
     }
-
-    const onSubmit = (e) => {
-        e.preventDefault();
-         onLoginSubmit(formValues)
-    }
-
-
-    return(
-       
-        <div className={styles['form-container']}>
+    else {
+        if (errors.email) {
+          setErrors((state) => ({ ...state, email: "" }));
+        }
+      }
+  };
 
 
-            <form onSubmit={onSubmit} method="POST">
+  // АКО искаме да има валидацяи за паролата
+  
+  // const passwordValidator = () => {
+  //   if (formValues.password.length < 6) {
+  //     setErrors((state) => ({
+  //       ...state,
+  //       password: "Паролата трябва да бъде минимум 6 символа!",
+  //     }));
+  //   } else {
+  //     if (errors.password) {
+  //       setErrors((state) => ({ ...state, password: "" }));
+  //     }
+  //   }
+  // };
 
-                <p className={styles['sign-on']}>Sign in</p>
+  return (
+    <div className={styles["form-container"]}>
+      <form
+        onSubmit={onSubmit}
+        method="POST"
+        id="request"
+        className={styles["formlog"]}
+      >
+        <p className={styles["sign-on"]}>Sign in</p>
 
-                
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" value={formValues.email} onChange={changeHandler} placeholder="Your email address" />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          value={formValues.email}
+          onChange={changeHandler}
+          placeholder="Your email address"
+          required
+          onBlur={emailValidator}
+        />
+        {errors.email && <p className={styles.errorMessage}>{errors.email}</p>}
 
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" value={formValues.password} onChange={changeHandler} placeholder="Your password" />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          value={formValues.password}
+          onChange={changeHandler}
+          placeholder="Your password"
+          // onBlur={passwordValidator}
+        />
+        {/* {errors.password && (
+          <p className={styles.errorMessage}>{errors.password}</p>
+        )} */}
+        <Link to="/">Forgot your password?</Link>
 
-                <Link to="/">Forgot your password?</Link>
+        <input type="submit" value="Sign in" />
 
-                <input type="submit" value="Sign in" />
-
-
-                <Link to="/register"  className={styles['create-account']}>Create account</Link>
-
-            </form>
-
-        </div>
-    )
-
-
+        <Link to="/register" className={styles["create-account"]}>
+          Create account
+        </Link>
+      </form>
+    </div>
+  );
 }
--- e-book-online-store/src/components/Register/Register.jsx
+++ e-book-online-store/src/components/Register/Register.jsx
@@ -1,65 +1,181 @@
-
-
-
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useAuthContext } from "../../contexts/AuthContext";
 
-import styles from './Register.module.css';
+import styles from "./Register.module.css";
 
+export default function Register() {
+  document.title = "Register Page";
 
-export default function Register () {
+  const { onRegisterSubmit } = useAuthContext();
 
-    const { onRegisterSubmit } = useAuthContext();
+  const [formValues, setFormValues] = useState({
+    first_last_name: "",
+    email: "",
+    password: "",
+    repassword: "",
+  });
 
-    const [formValues, setFormValues] = useState({
-        first_last_name: '',
-        email: '',
-        password: '',
-        repassword: '',
-    })
+  const [errors, setErrors] = useState({});
 
+  const changeHandler = (e) => {
+    setFormValues((state) => ({ ...state, [e.target.name]: e.target.value }));
+  };
 
-    const changeHandler = (e) => {
-        setFormValues( state => ({...state, [e.target.name]: e.target.value}))
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onRegisterSubmit(formValues);
+  };
+
+  // Validations
+
+  /////////////////////////////////////////////////////// FIRST AND LAST NAME VALIDATOR  //////////////////////////////////////////////////////
+
+  const first_Last_NameValidator = () => {
+    if (formValues.first_last_name.length < 2) {
+      setErrors((state) => ({
+        ...state,
+        username: "Това поле трябва да е повече от 2 символа!",
+      }));
+    } else {
+      if (errors.first_last_name) {
+        setErrors((state) => ({
+          ...state,
+          username: "",
+        }));
+      }
     }
+  };
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-         onRegisterSubmit(formValues);
+  // /////////////////////////////////// EMAIl Validator //////////////////////////
+
+  function emailIsValid(email) {
+    const regexEmail = /^[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]+$/;
+    return regexEmail.test(email);
+  }
+
+  const emailValidator = () => {
+    if (!emailIsValid(formValues.email)) {
+      setErrors((state) => ({
+        ...state,
+        email: "Посоченият от вас мейл адрес не е във валиден формат",
+      }));
+    } else {
+      if (errors.email) {
+        setErrors((state) => ({
+          ...state,
+          email: "",
+        }));
+      }
     }
+  };
 
+  ////////////////////////// PASSWORD VALIDATOR //////////////////////////////////////////////////////
 
-    return(
-       
-        <div className={styles['form-container']}>
+  const passwordValidator = () => {
+    if (formValues.password.length < 6) {
+      setErrors((state) => ({
+        ...state,
+        password: "Вашата парола трябва да бъде минимум 6 символа!",
+      }));
+    } else {
+      if (errors.password) {
+        setErrors((state) => ({
+          ...state,
+          password: "",
+        }));
+      }
+    }
+  };
 
+  /////////////////////////////////// REPASS VALIDATOR //////////////////////////////////////////////////////
 
-            <form onSubmit={onSubmit} method="POST">
+  const repassValidator = () => {
+    if (formValues.password != formValues.repassword) {
+      setErrors((state) => ({
+        ...state,
+        repassword: "Посочената парола не съвпада!",
+      }));
+    } else {
+      if (errors.repassword) {
+        setErrors((state) => ({
+          ...state,
+          repassword: "",
+        }));
+      }
+    }
+  };
 
-                <p className={styles['sign-up']}>Create account</p>
+  return (
+    <div className={styles["form-container"]}>
+      <form onSubmit={onSubmit} method="POST">
+        <p className={styles["sign-up"]}>Create account</p>
 
-                <label htmlFor="first_last_name">Your name</label>
-                <input type="text" name="first_last_name" value={formValues.username} onChange={changeHandler} placeholder="First and last name" />
-                
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" value={formValues.email} onChange={changeHandler} placeholder="Valid email address" />
+        <label htmlFor="first_last_name">Your name</label>
+        <input
+          type="text"
+          name="first_last_name"
+          value={formValues.username}
+          onChange={changeHandler}
+          placeholder="First and last name"
+          onBlur={first_Last_NameValidator}
+          required
+        />
+        {errors.username && (
+          <p className={styles.errorMessage}>{errors.username}</p>
+        )}
 
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" value={formValues.password} onChange={changeHandler} placeholder="At least 6 characters"/>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          value={formValues.email}
+          onChange={changeHandler}
+          placeholder="Valid email address"
+          onBlur={emailValidator}
+          id="email"
+        />
 
-                <label htmlFor="repassword">Confirm Password</label>
-                <input type="password" name="repassword" value={formValues.repassword} onChange={changeHandler} placeholder="Repeat your password" />
+        {errors.email && <p className={styles.errorMessage}>{errors.email}</p>}
 
-                <input type="submit" value="Sign on" />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          value={formValues.password}
+          onChange={changeHandler}
+          placeholder="At least 6 characters"
+          onBlur={passwordValidator}
+          id="password"
+          required
+        />
 
+        {errors.password && (
+          <p className={styles.errorMessage}>{errors.password}</p>
+        )}
 
-                <span>Aready have an account?  <Link to="/login">Sign in </Link></span>
+        <label htmlFor="repassword">Confirm Password</label>
+        <input
+          type="password"
+          name="repassword"
+          value={formValues.repassword}
+          onChange={changeHandler}
+          placeholder="Repeat your password"
+          onBlur={repassValidator}
+          id="repassword"
+          required
+        />
 
-            </form>
+        {errors.repassword && (
+          <p className={styles.errorMessage}>{errors.repassword}</p>
+        )}
 
-        </div>
-    )
+        <input type="submit" value="Sign on" />
 
-
+        <span>
+          Aready have an account? <Link to="/login">Sign in </Link>
+        </span>
+      </form>
+    </div>
+  );
 }
