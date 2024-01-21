eeb4f80f9c1a2568faab20efc4ec92d02e4c1edc
Rumen Raenkov <rumen_raenkov@abv.bg>
2024-01-21 13:34:29 +0200

    Upgrade NotFound Component

--- e-book-online-store/src/components/NotFound/NotFound.jsx
+++ e-book-online-store/src/components/NotFound/NotFound.jsx
@@ -1,12 +1,15 @@
 import styles from '../NotFound/NotFound.module.css'
+import { Link } from "react-router-dom";
 
 export default function NotFound(){
     document.title = 'Error';
     return(
         <>
-        <div className={styles['image']}>
-
-            <p>Грешка</p>
+        <div className={styles['not-found-container']}>
+            <img className={styles['image']}
+            src="images/fix-broken-links.png"
+            />      
+            <Link to={"/"} className={styles['home-link']}>Начало</Link>
         </div>
         </>
     );
